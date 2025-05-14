108d06a57c3fdd467524e1fb28cf2809d4c682f0
unknown <kdpavel8@gmail.com>
2025-05-14 21:44:38 +0500

    feat: download and connect libraries and base styles

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+
+import { BootstrapVue } from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import '@/assets/styles/fonts.css'
+import '@/assets/styles/main.scss'
 
 Vue.config.productionTip = false
 
+Vue.use(BootstrapVue)
+
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
