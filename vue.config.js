e3a3dd21c06e7a0e9028c6f8531ee8134eed9b44
unknown <kdpavel8@gmail.com>
2025-05-20 02:37:42 +0500

    deploy gh pages

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,10 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
 
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/olimpians/'
+    : '/',
+
   chainWebpack: (config) => {
     config.module.rules.delete('svg')
   },
