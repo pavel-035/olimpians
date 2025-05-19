8bb0b3c4c158b51e0f85b6850170ba4768af5dfa
unknown <kdpavel8@gmail.com>
2025-05-19 16:22:02 +0500

    feat(BaseSwiper.vue): connect swiper library

---
+++ src/components/BaseSwiper.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <div
+      ref="swiperRef"
+      class="swiper"
+    >
+      <div class="swiper-wrapper">
+        <div
+          v-for="slide in slides"
+          :key="slide.id"
+
+          class="swiper-slide"
+        >
+          <slot
+            name="slide"
+            :slide="slide"
+          ></slot>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Swiper from 'swiper'
+import { Navigation, Pagination } from 'swiper/modules'
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+
+export default {
+  name: 'BaseSwiper',
+
+  props: {
+    slides: {
+      type: Array,
+      required: true
+    },
+    options: {
+      type: Object,
+      default: () => ({})
+    }
+  },
+
+  mounted () {
+    return new Swiper(this.$refs.swiperRef, {
+      modules: [Navigation, Pagination],
+
+      ...this.options
+    })
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
